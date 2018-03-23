21f5629bb50553c9d741d93e6183d5bfd0e7ff57
LV <954777574@qq.com>
2018-03-23 21:13:43 +0800

    ###1.Push transition

--- Class/Util/LVNavigation/LVPushTransition.m
+++ Class/Util/LVNavigation/LVPushTransition.m
@@ -7,7 +7,41 @@
 //
 
 #import "LVPushTransition.h"
+#import <UIKit/UIScreen.h>
 
 @implementation LVPushTransition
+
+// This is used for percent driven interactive transitions, as well as for
+// container controllers that have companion animations that might need to
+// synchronize with the main animation.
+- (NSTimeInterval)transitionDuration:(nullable id <UIViewControllerContextTransitioning>)transitionContext {
+    return 0.8;
+}
+
+// This method can only  be a nop if the transition is interactive and not a percentDriven interactive transition.
+- (void)animateTransition:(id <UIViewControllerContextTransitioning>)transitionContext {
+    UIViewController * fromVC = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+    UIViewController * toVC = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    
+    CGRect finalFrameForVC = [transitionContext finalFrameForViewController:toVC];
+    CGRect bounds = [[UIScreen mainScreen] bounds];
+    toVC.view.frame = CGRectOffset(finalFrameForVC, -bounds.size.width, 0);
+    [[transitionContext containerView] addSubview:toVC.view];
+    
+    [UIView animateWithDuration:0.8
+                          delay:0.0
+         usingSpringWithDamping:0.5
+          initialSpringVelocity:0.0
+                        options:UIViewAnimationOptionCurveLinear
+                     animations:^{
+                         fromVC.view.alpha = 0.8;
+                         toVC.view.frame = finalFrameForVC;
+                     } completion:^(BOOL finished) {
+                         [transitionContext completeTransition:YES];
+                         fromVC.view.alpha = 1.0;
+                     }];
+    
+    NSLog(@"\n%@\n%@",fromVC,toVC);
+}
 
 @end
